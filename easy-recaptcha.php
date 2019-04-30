62999edfd03aa283e402ce1aa6cf515cd5f5c229
Russell Ramey <doublercreative@gmail.com>
2019-04-30 13:38:21 -0500

    initial commit, plugin setup

---
+++ easy-recaptcha.php
@@ -0,0 +1,9 @@
+<?php
+/***
+* Plugin Name: Easy ReCaptcha
+* Plugin URI: https://github.com/russellramey/easy-recaptcah
+* Description: Enable & append Google ReCaptcha API to default WP login
+* Version: 1.0
+* Author: Russell Ramey
+* Author URI: https://russellramey.me/
+***/
