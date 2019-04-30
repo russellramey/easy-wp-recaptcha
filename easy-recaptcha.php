e8e7d8e914b84839702da5c1e6a00f6e47f2aa6e
Russell Ramey <doublercreative@gmail.com>
2019-04-30 16:09:03 -0500

    added recaptcha process and validation function

--- easy-recaptcha.php
+++ easy-recaptcha.php
@@ -2,17 +2,22 @@
 /***
 * Plugin Name: Easy ReCaptcha
 * Plugin URI: https://github.com/russellramey/easy-recaptcah
-* Description: Enable & append Google ReCaptcha API to default WP login. Supports Google ReCaptcha v2.
+* Description: Enable & append Google ReCaptcha API to default WP login. Supports Google ReCaptcha v2 checkbox and invisible versions.
 * Version: 1.0
 * Author: Russell Ramey
 * Author URI: https://russellramey.me/
 ***/
+
+
+
+
 
 /***
 * Add scripts & styles to WP login page
 ***/
 add_action('login_enqueue_scripts', 'login_recaptcha_script');
 function login_recaptcha_script() {
+
 	// Register recaptcha script
 	wp_register_script('recaptcha-script', 'https://www.google.com/recaptcha/api.js');
 	// Enqueue registered recaptcha script
@@ -27,9 +32,52 @@ function login_recaptcha_script() {
 ***/
 add_action( 'login_form', 'display_recaptcha_on_login' );
 function display_recaptcha_on_login() {
+
 	// Append g-recaptcha element with required SiteKey
 	echo "<div class='g-recaptcha' data-sitekey='" . EASY_RECAPTCHA_SITE_KEY . "'></div>";
 
 	// Add simple JS script to make username and password fields required
 	echo "<script>document.getElementById('user_pass').setAttribute('required', 'required'); document.getElementById('user_login').setAttribute('required', 'required');</script>";
+}
+
+/***
+* Process ReCaptcha response on WP login submit action
+***/
+add_filter('wp_authenticate_user', 'verify_recaptcha_on_login', 10, 2);
+function verify_recaptcha_on_login($user, $password) {
+
+	// If recaptcha exists
+	if (isset($_POST['g-recaptcha-response'])) {
+
+		// Verify recaptcha with google
+		$response = wp_remote_get( 'https://www.google.com/recaptcha/api/siteverify?secret="' . EASY_RECAPTCHA_SECRET .  '"&response=' . $_POST['g-recaptcha-response'] );
+
+		// If response data is returned
+		if ($response){
+
+			// Decode response body
+			$response = json_decode($response['body'], true);
+
+			// If response is successful
+			if (true == $response['success']) {
+
+				// Return WP User object
+				return $user;
+
+			} else {
+
+				// Return error if ReCaptcha fail
+				return new WP_Error( 'Captcha Invalid', __('<strong>ERROR</strong>: Please provide the reCAPTCHA response.') );
+
+			}
+
+		}
+
+	} else {
+
+		// Return error if ReCaptcha is not submited with login attempt
+		return new WP_Error( 'Captcha Invalid 2', __('<strong>ERROR</strong>: Please provide the reCAPTCHA response.') );
+
+	}
+
 }
