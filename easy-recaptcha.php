2b0e4b6212ee59cb1862446b925cd5cc373d9f06
Russell Ramey <doublercreative@gmail.com>
2019-04-30 13:47:52 -0500

    added inital setup functions

--- easy-recaptcha.php
+++ easy-recaptcha.php
@@ -2,8 +2,34 @@
 /***
 * Plugin Name: Easy ReCaptcha
 * Plugin URI: https://github.com/russellramey/easy-recaptcah
-* Description: Enable & append Google ReCaptcha API to default WP login
+* Description: Enable & append Google ReCaptcha API to default WP login. Supports Google ReCaptcha v2.
 * Version: 1.0
 * Author: Russell Ramey
 * Author URI: https://russellramey.me/
 ***/
+
+/***
+* Add scripts & styles to WP login page
+***/
+add_action('login_enqueue_scripts', 'login_recaptcha_script');
+function login_recaptcha_script() {
+	// Register recaptcha script
+	wp_register_script('recaptcha-script', 'https://www.google.com/recaptcha/api.js');
+	// Enqueue registered recaptcha script
+	wp_enqueue_script('recaptcha-script');
+
+	// Register login styles
+	wp_enqueue_style( 'easy-recaptcha-styles', get_stylesheet_directory_uri() . '/css/login-style.css' );
+}
+
+/***
+* Add reuiqred elements to WP login form
+***/
+add_action( 'login_form', 'display_recaptcha_on_login' );
+function display_recaptcha_on_login() {
+	// Append g-recaptcha element with required SiteKey
+	echo "<div class='g-recaptcha' data-sitekey='" . EASY_RECAPTCHA_SITE_KEY . "'></div>";
+
+	// Add simple JS script to make username and password fields required
+	echo "<script>document.getElementById('user_pass').setAttribute('required', 'required'); document.getElementById('user_login').setAttribute('required', 'required');</script>";
+}
